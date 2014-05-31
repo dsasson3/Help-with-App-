ee235ca80c5a0bc9b9f8c83b7d61ec31acb8fa42
dsasson3 <danielsasson3@gmail.com>
2014-05-31 19:53:31 -0400

    Create MainViewController.h

---
+++ MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  new
+//
+//  Created by Daniel Sasson on 5/31/14.
+//  Copyright (c) 2014 Daniel Sasson. All rights reserved.
+//
+
+#import "AMSlideMenuMainViewController.h"
+
+@interface MainViewController : AMSlideMenuMainViewController
+
+@end
